e9e4e89fd94ee950ab2bed4247535777b116e301
Galore Consultants <dashtech1@gmail.com>
2021-02-03 18:41:26 -0500

    created invert.rb

---
+++ invert.rb
@@ -0,0 +1,16 @@
+# Create a function which takes an array of number arr and returns inverted array.
+
+
+
+
+
+
+
+
+
+
+p invert_array([1, 2, 3, 4, 5])) ➞ [-1, -2, -3, -4, -5]
+
+p invert_array([1, -2, 3, -4, 5] ➞ [-1, 2, -3, 4, -5]
+
+p invert_array([]) ➞ []
