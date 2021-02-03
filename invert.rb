723fdd5f561b2a3e7d903ea273011e653d0f1998
Galore Consultants <dashtech1@gmail.com>
2021-02-03 18:56:33 -0500

    finished invert

--- invert.rb
+++ invert.rb
@@ -1,5 +1,12 @@
 # Create a function which takes an array of number arr and returns inverted array.
 
+def invert_array(arr)
+
+arr.map! do |ele|
+	(ele - ele) - ele
+end
+
+end
 
 
 
@@ -8,9 +15,8 @@
 
 
 
+p invert_array([1, 2, 3, 4, 5]) == [-1, -2, -3, -4, -5]
 
-p invert_array([1, 2, 3, 4, 5])) ➞ [-1, -2, -3, -4, -5]
+p invert_array([1, -2, 3, -4, 5]) == [-1, 2, -3, 4, -5]
 
-p invert_array([1, -2, 3, -4, 5] ➞ [-1, 2, -3, 4, -5]
-
-p invert_array([]) ➞ []
+ p invert_array([])
