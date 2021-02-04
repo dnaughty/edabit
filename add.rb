8ddad98edaa609f29eca030378374219b278d843
Galore Consultants <dashtech1@gmail.com>
2021-02-03 20:30:15 -0500

    Finished add.rb

--- add.rb
+++ add.rb
@@ -1,8 +1,6 @@
 def addition(num)
 
 		num + 1
-
-
 end
 
 
@@ -14,5 +12,4 @@ end
 
 p addition(0) == 1
 p addition(9) == 10
-
 p addition(-3) ==-2
---
+++ sum.rb
@@ -0,0 +1,21 @@
+def addition(x,y)
+
+x + y
+
+
+
+end
+
+
+
+
+
+
+
+
+
+p addition(3, 2) == 5
+
+p addition(-3, -6) == -9
+
+p addition(7, 3) == 10
