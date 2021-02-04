0e6cf41bffd16ea5325515c6ddb432f0cf60e61a
Galore Consultants <dashtech1@gmail.com>
2021-02-03 20:38:26 -0500

    Finished sum_minimums.rb

--- sum_minimums.rb
+++ sum_minimums.rb
@@ -1,6 +1,14 @@
-sum_minimums(arr)
+def sum_minimums(arr)
+
+sum = 0
+arr.each do |x|
+	sum += x.sort[0]
+
+end
 
 
+
+sum
 
 
 
@@ -11,12 +19,10 @@ end
 
 
 
-sum_minimums([[1, 2, 3, 4, 5],
+p sum_minimums([[1, 2, 3, 4, 5],
   [5, 6, 7, 8, 9],
   [20, 21, 34, 56, 100]
-])➞ 26
+]) == 26
 
 # minimum value of first row is 1
 # minimum value of second row is 5
-# minimum value of third row is 20
-Notes
